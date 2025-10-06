664d7531e8d01af94e18bfa6680c190cdcbd313e
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-10-06 17:09:12 +0200

    Burger menu done.

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,30 @@
 import CustomNavLink from "../CustomNavLink/CustomNavLink";
 import "../../styles/components/Header.scss";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [showPanel, setShowPanel] = useState(false);
+
+  useEffect(() => {
+    const mq = window.matchMedia("(min-width: 768px)");
+    console.warn(mq);
+    const listener = (e: MediaQueryListEvent) =>
+      e.matches && setShowPanel(false);
+    mq.addEventListener("change", listener);
+    return () => mq.removeEventListener("change", listener);
+  }, []);
+
   return (
     <>
       <header>
-        <nav>
-          <ul className="header-nav-ul">
+        <nav className="header-nav">
+          <img
+            className="header-nav-burger-bar"
+            src="burger-bar.png"
+            alt=""
+            onClick={() => setShowPanel(!showPanel)}
+          />
+          <ul className={`header-nav-ul ${showPanel ? "open" : ""}`}>
             <li>
               <CustomNavLink to="/">Home</CustomNavLink>
             </li>
@@ -15,7 +33,7 @@ export default function Header() {
             </li>
           </ul>
         </nav>
-        <h1>MarkupFlow</h1>
+        <h1 className="header-title">MarkupFlow</h1>
       </header>
     </>
   );
