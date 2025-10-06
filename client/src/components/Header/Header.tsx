0980982edd2bc859976452894011a56405f16a37
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2025-10-06 23:03:25 +0200

    Work in progress.

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,15 +5,6 @@ import { useEffect, useState } from "react";
 export default function Header() {
   const [showPanel, setShowPanel] = useState(false);
 
-  useEffect(() => {
-    const mq = window.matchMedia("(min-width: 768px)");
-    console.warn(mq);
-    const listener = (e: MediaQueryListEvent) =>
-      e.matches && setShowPanel(false);
-    mq.addEventListener("change", listener);
-    return () => mq.removeEventListener("change", listener);
-  }, []);
-
   return (
     <>
       <header>
